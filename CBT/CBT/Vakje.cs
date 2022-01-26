42256f7b4fe138a5b2cff04cbfade69222c7943b
TheNickTestGit <74415397+TheNickTestGit@users.noreply.github.com>
2022-01-26 20:46:45 +0100

    Start
    De vorige code opgeruimd en klaar gemaakt om op gebouwd te worden.

---
+++ CBT/CBT/Vakje.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+class Vakje
+{
+    public int waarde;
+    public int[] domein;
+
+    public Vakje(int waarde = 0, int[] domein = null)
+    {
+        this.waarde = waarde;
+
+        if (domein == null)
+            this.domein = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        else
+            this.domein = domein;
+    }
+}
+
